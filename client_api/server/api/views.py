9475090f0d7879b8743136d3a5bb9b21fa98d8f8
Levan Gogohia <levangogohia@swift-coder.local>
2019-06-08 16:13:27 +0300

    f

--- client_api/server/api/views.py
+++ client_api/server/api/views.py
@@ -1,10 +1,14 @@
-import json
+from django.shortcuts import render
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import json
+
+# Create your views here.
 
 
 class GeoList(APIView):
 
-    def get(self):
+    def get(self, request):
         with open('../test.json', 'r') as f:
             return Response(json.load(f))
