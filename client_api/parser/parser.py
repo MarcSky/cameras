245cdc158e154b10d6055f6e8396fb2365ba41cc
Levan Gogohia <levangogohia@swift-coder.local>
2019-06-09 07:59:30 +0300

    fix unique geometry

--- client_api/parser/parser.py
+++ client_api/parser/parser.py
@@ -1,12 +1,24 @@
 import shapefile
 
-sf = shapefile.Reader("data/buildings/buildings.shp")
+file = "data/green/green.shp"
+# sf = shapefile.Reader("data/green/green.shp")
 # sf = shapefile.Reader("data/ntopoly/ntopoly.shp")
-shapes = sf.shapes()
-for i in range(len(shapes)):
-    print(type(shapes[i]))
-    print(shapes[i].shapeType)
-    print(shapes[i].parts)
-    print(shapes[i].points)
-    print(type(shapes[i].points))
+# shapes = sf.shapes()
+# print(len(shapes))
+# for i in range(len(shapes)):
+#     print(type(shapes[i]))
+#     print(shapes[i].shapeType)
+#     print(shapes[i].parts)
+#     print(shapes[i].points)
+#     print(type(shapes[i].points))
 
+
+from osgeo import ogr
+file = ogr.Open(file)
+shape = file.GetLayer(0)
+#first feature of the shapefile
+feature = shape.GetFeature(0)
+
+# first = feature.ExportToJson()
+print (feature) # (GeoJSON format)
+# {"geometry": {"type": "LineString", "coordinates": [[0.0, 0.0], [25.0, 10.0], [50.0, 50.0]]}, "type": "Feature", "properties": {"FID": 0.0}, "id": 0}
---
+++ client_api/test-graph_polygon_geojson.py
@@ -0,0 +1,74 @@
+import PIL.Image as Image
+import PIL.ImageDraw as ImageDraw
+from shapely.geometry import LineString, Point, Polygon
+
+from server.camera_utils.camera import Camera
+
+WORLD = (
+    (50, 50),
+    (50, 950),
+    (950, 950),
+    (950, 50),
+
+)
+DATA = [
+    (
+        (100, 100),
+        (100, 800),
+        (200, 800),
+        (200, 100),
+    ),
+    (
+        (800, 100),
+        (800, 800),
+        (900, 800),
+        (900, 100),
+    ),
+    (
+        (500, 100),
+        (500, 300),
+        (600, 300),
+        (600, 100),
+    ),
+    (
+        (500, 700),
+        (500, 800),
+        (600, 800),
+        (600, 700),
+    ),
+    (
+        (500, 400),
+        (500, 500),
+        (600, 500),
+        (600, 400),
+    ),
+]
+
+# image = Image.new("RGB", size=(1200, 1000), color=(255,255,255,0))
+#
+# draw = ImageDraw.Draw(image)
+#
+# for start, end in tuple(zip(WORLD[:-1], WORLD[1:])):
+#     draw.line(start + end, fill=128, width=3)
+
+cameras = []
+
+c = Camera(Point(1000, 400), LineString([(0, 0), (1, 1)]))
+c.refresh_polygon()
+cameras.append(c)
+
+c = Camera(Point(800, 600), LineString([(0, 0), (-3, -1)]))
+c.refresh_polygon()
+cameras.append(c)
+
+for points in DATA:
+    for c in cameras:
+        c.screen_building(Polygon(points))
+
+polygons = []
+for c in cameras:
+    polygons.append(c.polygon)
+
+print(polygons)
+
+# def polygonsToGeoJson(polygons):
